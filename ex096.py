c4c14ef719a46e72f3d1cec41ce7608bb1e99af0
gabrielaPezzini <gabrielacachapuz2021@gmail.com>
2023-04-06 19:11:58 -0300

    Aula 20 - Funções (Parte 1)

---
+++ ex096.py
@@ -0,0 +1,10 @@
+def área(larg, comp):
+    a = larg * comp
+    print(f'A área de um terreno {larg}X{comp} é de {a}m².')
+
+#Programa principal
+print('Controle de Terrenos')
+print('-' * 30)
+l = float(input('LARGURA (m): '))
+c = float(input('COMPRIMENTO (m): '))
+área(l, c)
