46115a98c29d6bda8041f6c988bf5973f1b96bc3
gabrielaPezzini <gabrielacachapuz2021@gmail.com>
2023-04-19 17:04:37 -0300

    Aula 21 - Funções (Parte 2)

---
+++ ex105.py
@@ -0,0 +1,26 @@
+def notas(*n, sit=False): # o * significa possibilitar aplicar várias notas.
+    """
+    -> Função para analisar notas e situações de vários alunos.
+    :param n: uma ou mais notas dos alunos (aceita várias)
+    :param sit: valor opcional, indicandp se deve ou não adicionar a situação
+    :return: dicionário com várias informações sobre a situação da turma.
+    """
+    r = dict()
+    r['total'] = len(n)
+    r['maior'] = max(n)
+    r['menor'] = min(n)
+    r['média'] = sum(n) / len(n)
+    if sit: # se a situação for verdadeira
+        if r['média'] >= 7:
+            r['situação'] = 'BOA'
+        elif r['média'] >= 5:
+            r['situação'] = 'RAZOÁVEL'
+        else:
+            r['situação'] = 'RUIM'
+    return r
+
+
+# Programa principal
+resp = notas(10, 5.5, 2.5, 9, 8.5, sit=True)
+print(resp)
+help(notas)
